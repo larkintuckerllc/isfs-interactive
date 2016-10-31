bc6216ac3c22646d45d7586255d03e561eefd737
John Tucker <john@larkintuckerllc.com>
2016-10-30 21:15:50 -0400

    Cleaned Up

--- src/components/Saver/index.jsx
+++ src/components/Saver/index.jsx
@@ -4,10 +4,7 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import * as fromReactRouterRedux from 'react-router-redux';
 import * as fromMapView from '../../ducks/mapView';
-import { getDrawingOpen } from '../../ducks/drawingOpen';
 import * as fromTile from '../../ducks/tile';
-import * as fromTilesOpen from '../../ducks/tilesOpen';
-import * as fromLayersOpen from '../../ducks/layersOpen';
 import {
   frameXYToContentXY,
   getContentHeight,
@@ -16,17 +13,9 @@ import {
   getFrameWidth,
   getScale,
 } from '../../util/grid';
-import { getZoomMin } from '../../util/parameters';
-import { HAND_WIDTH, MAX_LAT, MIN_LAT, ZOOM_MAX } from '../../config';
 import styles from './index.scss';
 
 class Saver extends Component {
-  constructor() {
-    super();
-    this.handleTouchStart = this.handleTouchStart.bind(this);
-    this.handleTouchMove = this.handleTouchMove.bind(this);
-    this.handleTouchEnd = this.handleTouchEnd.bind(this);
-  }
   componentWillMount() {
     const { mapView, tile } = this.props;
     this.contentCenterX = getContentWidth() / 2;
@@ -63,11 +52,6 @@ class Saver extends Component {
     frameContentContainPositionEl.id = styles.frameContentContainPosition;
     this.frameContentContainEl.appendChild(frameContentContainPositionEl);
     this.position = L.map(styles.frameContentContainPosition);
-    // EVENT LISTENERS
-    this.frameContentContainEl.addEventListener('touchstart', this.handleTouchStart, true);
-    this.frameContentContainEl.addEventListener('touchmove', this.handleTouchMove, true);
-    this.frameContentContainEl.addEventListener('touchend', this.handleTouchEnd, true);
-    this.frameContentContainEl.addEventListener('touchcancel', this.handleTouchEnd, true);
     this.positionMap(mapView);
     this.changeTile(tile);
   }
@@ -77,149 +61,9 @@ class Saver extends Component {
     this.positionMap(mapView);
   }
   componentWillUnmount() {
-    this.frameContentContainEl.removeEventListener('touchstart', this.handleTouchStart);
-    this.frameContentContainEl.removeEventListener('touchmove', this.handleTouchMove);
-    this.frameContentContainEl.removeEventListener('touchend', this.handleTouchEnd);
-    this.frameContentContainEl.removeEventListener('touchcancel', this.handleTouchEnd);
     this.map.remove();
     this.position.remove();
     this.frameContentEl.removeChild(this.frameContentContainEl);
-  }
-  handleTouchStart(e) {
-    e.stopPropagation();
-    if (e.touches.length !== 1) return;
-    this.moving = true;
-    this.zooming = false;
-    this.touchOneLastX = (e.touches[0].pageX * this.scale) + this.visibleContentLeft;
-    this.touchOneLastY = (e.touches[0].pageY * this.scale) + this.visibleContentTop;
-  }
-  handleTouchMove(e) {
-    e.stopPropagation();
-    if (!this.moving) return;
-    if (this.zooming) return;
-    let radius = 0;
-    for (let i = 0; i < e.touches.length; i++) {
-      for (let j = 0; j < e.touches.length; j++) {
-        if (i < j) {
-          radius = Math.max(Math.floor(this.scale * Math.sqrt(
-            Math.pow(e.touches[i].pageX - e.touches[j].pageX, 2) +
-            Math.pow(e.touches[i].pageY - e.touches[j].pageY, 2)
-          )), radius);
-        }
-      }
-    }
-    if (radius >= HAND_WIDTH) {
-      this.moving = false;
-      this.zooming = true;
-      this.startRadius = radius;
-    }
-    const { mapView, setMapView } = this.props;
-    const touchOneX = (e.touches[0].pageX * this.scale) + this.visibleContentLeft;
-    const touchOneY = (e.touches[0].pageY * this.scale) + this.visibleContentTop;
-    const shiftY = this.touchOneLastY - touchOneY;
-    let atBoundary = false;
-    const topLeftLatLng = this.position.containerPointToLatLng(
-      L.point(0, 0)
-    );
-    const bottomRightLatLng = this.position.containerPointToLatLng(
-      L.point(this.contentCenterX * 2, this.contentCenterY * 2)
-    );
-    if (topLeftLatLng.lat > MAX_LAT && shiftY < 0) {
-      atBoundary = true;
-    }
-    if (bottomRightLatLng.lat < MIN_LAT && shiftY > 0) {
-      atBoundary = true;
-    }
-    const centerLatLng = this.position.containerPointToLatLng(
-      L.point(
-        this.contentCenterX + (this.touchOneLastX - touchOneX),
-        this.contentCenterY + shiftY
-      )
-    );
-    setMapView({
-      center: {
-        lat: atBoundary ? mapView.center.lat : centerLatLng.lat,
-        lng: centerLatLng.lng,
-      },
-      zoom: mapView.zoom,
-    });
-    this.touchOneLastX = touchOneX;
-    this.touchOneLastY = touchOneY;
-  }
-  handleTouchEnd(e) {
-    e.stopPropagation();
-    if (this.moving && e.touches.length > 0) {
-      this.touchOneLastX = (e.touches[0].pageX * this.scale) + this.visibleContentLeft;
-      this.touchOneLastY = (e.touches[0].pageY * this.scale) + this.visibleContentTop;
-    }
-    if (!this.zooming) return;
-    const { mapView, setMapView } = this.props;
-    let radius = 0;
-    let zoom = mapView.zoom;
-    let center = mapView.center;
-    let totalX = (e.changedTouches[0].pageX * this.scale) + this.visibleContentLeft;
-    let totalY = (e.changedTouches[0].pageY * this.scale) + this.visibleContentTop;
-    this.zooming = false;
-    for (let i = 0; i < e.touches.length; i++) {
-      totalX += (e.touches[i].pageX * this.scale) + this.visibleContentLeft;
-      totalY += (e.touches[i].pageY * this.scale) + this.visibleContentTop;
-      for (let j = 0; j < e.touches.length; j++) {
-        if (i < j) {
-          radius = Math.max(Math.floor(this.scale * Math.sqrt(
-            Math.pow(e.touches[i].pageX - e.touches[j].pageX, 2) +
-            Math.pow(e.touches[i].pageY - e.touches[j].pageY, 2)
-          )), radius);
-        }
-      }
-      radius = Math.max(Math.floor(this.scale * Math.sqrt(
-        Math.pow(e.touches[i].pageX - e.changedTouches[0].pageX, 2) +
-        Math.pow(e.touches[i].pageY - e.changedTouches[0].pageY, 2)
-      )), radius);
-    }
-    if (radius >= this.startRadius && mapView.zoom < ZOOM_MAX) {
-      const centerLatLng = this.position.containerPointToLatLng(
-        L.point(
-          ((totalX / (e.touches.length + 1)) + this.contentCenterX) / 2,
-          ((totalY / (e.touches.length + 1)) + this.contentCenterY) / 2
-        )
-      );
-      center = {
-        lat: centerLatLng.lat,
-        lng: centerLatLng.lng,
-      };
-      zoom = mapView.zoom + 1;
-    }
-    if (radius <= this.startRadius && mapView.zoom > getZoomMin()) {
-      const topLeftLatLng = this.position.containerPointToLatLng(
-        L.point(0, 0)
-      );
-      const bottomRightLatLng = this.position.containerPointToLatLng(
-        L.point(this.contentCenterX * 2, this.contentCenterY * 2)
-      );
-      if (topLeftLatLng.lat >= 0 && bottomRightLatLng.lat >= 0) {
-        center = {
-          lat: bottomRightLatLng.lat,
-          lng: mapView.center.lng,
-        };
-      }
-      if (topLeftLatLng.lat >= 0 && bottomRightLatLng.lat < 0) {
-        center = {
-          lat: 0,
-          lng: mapView.center.lng,
-        };
-      }
-      if (topLeftLatLng.lat < 0 && bottomRightLatLng.lat < 0) {
-        center = {
-          lat: topLeftLatLng.lat,
-          lng: mapView.center.lng,
-        };
-      }
-      zoom = mapView.zoom - 1;
-    }
-    setMapView({
-      center,
-      zoom,
-    });
   }
   positionMap(view) {
     this.position.setView(
@@ -254,32 +98,20 @@ class Saver extends Component {
   }
 }
 Saver.propTypes = {
-  children: PropTypes.node,
-  drawingOpen: PropTypes.bool.isRequired,
-  layersOpen: PropTypes.bool.isRequired,
-  location: PropTypes.object.isRequired,
   mapView: PropTypes.object.isRequired,
   push: PropTypes.func.isRequired,
-  setLayersOpen: PropTypes.func.isRequired,
   setMapView: PropTypes.func.isRequired,
   setTile: PropTypes.func.isRequired,
-  setTilesOpen: PropTypes.func.isRequired,
   tile: PropTypes.object.isRequired,
-  tilesOpen: PropTypes.bool.isRequired,
 };
 export default connect(
   state => ({
-    drawingOpen: getDrawingOpen(state),
-    layersOpen: fromLayersOpen.getLayersOpen(state),
     mapView: fromMapView.getMapView(state),
     tile: fromTile.getTile(state),
-    tilesOpen: fromTilesOpen.getTilesOpen(state),
   }),
   {
     push: fromReactRouterRedux.push,
-    setLayersOpen: fromLayersOpen.setLayersOpen,
     setMapView: fromMapView.setMapView,
     setTile: fromTile.setTile,
-    setTilesOpen: fromTilesOpen.setTilesOpen,
   }
 )(Saver);
